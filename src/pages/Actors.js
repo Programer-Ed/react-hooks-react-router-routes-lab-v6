67a96bc02bbc568df04ca731784cd1efa5b499c8
Edwin <moyiedwin8@gmail.com>
2024-08-20 00:23:47 +0300

    routing done

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Actors() {
+  const [actors,setActors] =useState([])
+  useEffect(() => {
+   fetch('http://localhost:8000/actors')
+   .then(response => response.json())
+   .then(data => setActors(data))
+   .catch(error => console.error(error))
+  },[])
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar/>
+        <h1>Actors Page</h1>
       </header>
       <main>
-        {/* Actor info here! */}
+      {actors.map(actor => (
+          <article key={actor.name}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map(movie => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
+        {/* {actors.map(actor =>
+         <MovieCard key={actor.id} 
+         name={actor.name} 
+         movies={actor.movies}/>)} */}
       </main>
     </>
   );
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors,setDirectors] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:8000/directors")
+    .then(response => response.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  },[])
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
+        <h1>Directors Page</h1>
       </header>
       <main>
-        {/* Director info here! */}
+       
+          {directors.map(director => (
+          <article key={director.name}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map(movie => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
